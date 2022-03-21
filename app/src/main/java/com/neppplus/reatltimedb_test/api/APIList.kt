fb06994ab65ad7ac22b4746f59887c4ba07a4910
nepp.plus <nepp.plus@gmail.com>
2022-03-21 16:40:45 +0900

    기본 세팅 진행중

---
+++ app/src/main/java/com/neppplus/reatltimedb_test/api/APIList.kt
@@ -0,0 +1,101 @@
+package com.neppplus.reatltimedb_test.api
+
+import com.neppplus.reatltimedb_test.datas.BasicResponse
+import org.json.JSONObject
+import retrofit2.Call
+import retrofit2.http.*
+
+interface APIList {
+
+//    BASE_URL에 해당하는 서버에서, 어떤 기능들을 사용할건지 명시.
+
+    @FormUrlEncoded // 파라미터중에, Field (formData)에 담아야하는 파라미터가 있다면 필요한 구문.
+    @POST("/user")
+    fun postRequestLogin(
+        @Field("email") email: String,
+        @Field("password") pw: String
+    ) : Call<BasicResponse>  // 서버가 주는 응답을 (성공시에) BasicResponse 형태로 자동 파싱
+
+
+    @FormUrlEncoded
+    @PUT("/user")
+    fun putRequestSignUp(
+        @Field("email") email: String,
+        @Field("password") pw: String,
+        @Field("nick_name") nick: String,
+    ) : Call<BasicResponse>
+
+    @GET("/user")
+    fun getRequestMyInfo() : Call<BasicResponse>
+
+    @GET("/user/check")
+    fun getRequestDuplicatedCheck(
+        @Query("type") type: String,
+        @Query("value") value: String,
+    ) : Call<BasicResponse>
+
+    @GET("/user/friend")
+    fun getRequestFriendList(
+        @Query("type") type: String, // all, my, requested 세 문구 외에는 넣지 말자.
+    ) : Call<BasicResponse>
+
+    @GET("/search/user")
+    fun getRequestSearchUser(
+        @Query("nickname") nickname: String,
+
+    ) : Call<BasicResponse>
+
+    @FormUrlEncoded
+    @POST("/user/friend")
+    fun postRequestAddFriend(
+        @Field("user_id") userId: Int,
+    ) : Call<BasicResponse>
+
+    @FormUrlEncoded
+    @PUT("/user/friend")
+    fun putRequestAcceptOrDenyFriendRequest(
+        @Field("user_id") userId: Int,
+        @Field("type") type: String,
+    ) : Call<BasicResponse>
+
+    @FormUrlEncoded
+    @POST("/appointment")
+    fun postRequestAddAppointment(
+        @Field("title") title: String,
+        @Field("datetime") datetime: String,
+        @Field("start_place") startPlaceName: String,
+        @Field("start_latitude") startLat: Double,
+        @Field("start_longitude") startLng: Double,
+        @Field("place") place: String,
+        @Field("latitude") lat: Double,
+        @Field("longitude") lng: Double,
+    ) : Call<BasicResponse>
+
+    @GET("/appointment")
+    fun getRequestAppointmentList() : Call<BasicResponse>
+
+
+    @GET("/user/place")
+    fun getRequestMyPlaceList() : Call<BasicResponse>
+
+
+    @FormUrlEncoded
+    @POST("/user/place")
+    fun postRequestAddMyPlace(
+        @Field("name")  name: String,
+        @Field("latitude")  lat: Double,
+        @Field("longitude") lng: Double,
+        @Field("is_primary") isPrimary: Boolean
+    ) : Call<BasicResponse>
+
+
+    @FormUrlEncoded
+    @POST("/user/social")
+    fun postRequestSocialLogin(
+        @Field("provider") provider: String,
+        @Field("uid") uid: String,
+        @Field("nick_name") nickname: String,
+    ) : Call<BasicResponse>
+
+
+}
---
+++ app/src/main/java/com/neppplus/reatltimedb_test/api/ServerAPI.kt
@@ -0,0 +1,82 @@
+package com.neppplus.reatltimedb_test.api
+
+import android.content.Context
+import com.google.gson.GsonBuilder
+import com.neppplus.reatltimedb_test.utils.ContextUtil
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.*
+
+class ServerAPI {
+
+//    Retrofit 클래스 객체화 함수 => 단일 객체만 만들어서, 모든 화면에서 공유.
+//     여러개의 객체를 만들 필요 X. SingleTon 패턴
+
+   companion object {
+
+//       서버통신담당 클래스 : 레트로핏 클래스 객체를 담을 변수.
+//         아직 안만들었다면? 새로 만들고, 만들어둔게 있다면? 있는 retrofit 재활용.
+
+       private var retrofit : Retrofit? = null
+       private val BASE_URL = "https://keepthetime.xyz"
+
+       fun getRetrofit(context: Context) : Retrofit {
+
+//           Retrofit 라이브러리는, 클래스 차원에서 BASE_URL을 설정할 수 있게 도와줌.
+//           Retrofit + Gson 두개의 라이브러리를 결합하면 => JSON 파싱이 쉬워짐.
+
+           if (retrofit == null) {
+
+//               retrofit 객체 생성시에, 추가 세팅.
+
+//                자동으로 토큰을 첨부하도록.
+//                retrofit 변수를 통해서 API통신을 시작하기 직전에, 통신 정보를 먼저 가로챈다.
+//                가로챈 통신 정보에서, 무조건 헤더에 토큰을 첨부해두고, 나머지 작업을 이어가도록.
+
+               val interceptor = Interceptor {
+                   with(it) {
+
+//                       기존의 request에, 헤더를 추가해주자.
+
+                       val newRequest = request().newBuilder()
+                           .addHeader("X-Http-Token", ContextUtil.getLoginUserToken(context))
+                           .build()
+
+//                       다시 하려던 일을 이어가도록
+                       proceed(newRequest)
+                   }
+               }
+
+//               만들어낸 인터셉터를 활용하도록 세팅.
+//               레트로핏이 사용하는 클라이언트 객체를 수정.
+
+               val myClient = OkHttpClient.Builder()
+                   .addInterceptor(interceptor)
+                   .build()
+
+//               Date 자료형으로 파싱 => String을 yyyy-MM-dd HH:mm:ss 으로 파싱해서 저장해야함. (고정된 양식으로 내려줌)
+
+               val gson = GsonBuilder()
+                   .setDateFormat("yyyy-MM-dd HH:mm:ss")  // 서버가 이런 양식으로 보내주는 String을
+                   .registerTypeAdapter(
+                       Date::class.java,
+                       DateDeserializer()
+                   ) // 어떤 형태의 자료형에 적용시킬지?  Date클래스로 파싱.
+                   .create()
+
+               retrofit = Retrofit.Builder()
+                   .baseUrl(BASE_URL) // 어느 서버를 기반으로 움직일건지.
+                   .addConverterFactory( GsonConverterFactory.create( gson ) ) // gson 라이브러리와 결합 + Date 파싱 요령 첨부
+                   .client(myClient)  // 인터셉터를 부착해둔 클라이언트로 통신하도록.
+                   .build()
+           }
+
+           return retrofit!!
+
+       }
+
+    }
+
+}
